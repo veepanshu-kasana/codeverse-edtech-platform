28106f2bc194d19a46b963c23660cee0092bae7a
veepanshu-kasana <veepanshukasana2728@gmail.com>
2025-01-10 21:42:04 +0530

    feat: add initial home page layout

---
+++ src/components/core/HomePage/HighlightText.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const HighlightText = ({text}) => {
+  return (
+    <span className='font-bold text-blue-200'>
+        {" "}
+        {text}
+    </span>
+  )
+}
