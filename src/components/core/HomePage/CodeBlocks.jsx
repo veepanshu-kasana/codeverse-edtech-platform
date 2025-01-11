0e3cdc39861ece1b5d7e702e073c86f324da716a
veepanshu-kasana <veepanshukasana2728@gmail.com>
2025-01-11 23:53:23 +0530

    feat: integrate type animation package

--- src/components/core/HomePage/CodeBlocks.jsx
+++ src/components/core/HomePage/CodeBlocks.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { CTAButton } from './Button';
 import { HighlightText } from './HighlightText';
 import { FaArrowRight } from 'react-icons/fa';
+import { TypeAnimation } from 'react-type-animation';
 
 export const CodeBlocks = ({
     position, heading, subheading, ctabtn1, ctabtn2, codeblock, backgroundGradient, codeColor
@@ -32,6 +33,27 @@ export const CodeBlocks = ({
             
         </div>
 
+        {/* Section 2 */}
+        <div>
+            {/* Add BG Gradient -HW */}
+            <div className='text-center flex flex-col w-[10%] text-richblack-400 font-inter font-bold'>
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
+                <p>4</p>
+                <p>5</p>
+                <p>6</p>
+                <p>7</p>
+                <p>8</p>
+                <p>9</p>
+                <p>10</p>
+                <p>11</p>
+            </div>
+
+            <div className={`w-[90%] flex flex-col gap-2 font-bold font-mono ${codeColor} pr-2`}>
+                <TypeAnimation/>
+            </div>
+        </div>
     </div>
   )
 }
